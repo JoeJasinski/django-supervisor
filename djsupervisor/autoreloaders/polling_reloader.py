fddc869e41fc522833e4211662a4d36dac472713
Joe Jasinski <joe.jasinski@gmail.com>
2011-09-23 01:53:41 -0500

    bugfix.  still more to do

--- djsupervisor/autoreloaders/polling_reloader.py
+++ djsupervisor/autoreloaders/polling_reloader.py
@@ -4,7 +4,7 @@ from djsupervisor.autoreloaders.base import AutoReloader
 class PollingAutoReloader(AutoReloader):
 
     def run(self):
-
+        mtimes = {}
 	while True:
             if self._code_has_changed(self._live_dirs,mtimes):
 	    #  Fork a subprocess to make the restart call.
@@ -13,4 +13,22 @@ class PollingAutoReloader(AutoReloader):
                     self._command.handle("restart",*self._reload_progs,**self._options)
                 return 0
 	    time.sleep(1)
+
+
+    def _code_has_changed(self,live_dirs,mtimes):
+        """Check whether code under the given directories has changed.
+
+        This is a simple check based on file mtime.  New or deleted files
+        don't count as code changes.
+        """
+        for filepath in self._command._find_live_code_files(live_dirs):
+            try:
+                stat = os.stat(filepath)
+            except EnvironmentError:
+                continue
+            if filepath not in mtimes:
+                mtimes[filepath] = stat.st_mtime
+            else:
+                if mtimes[filepath] != stat.st_mtime:
+                    return True
 
